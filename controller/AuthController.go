c3d3ac435ef3de65ba784d546e28d9bca34e3628
deanchristt <alfadan60@gmail.com>
2022-11-04 11:22:29 +0700

    test push with tools

--- controller/AuthController.go
+++ controller/AuthController.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"fmt"
 	"github.com/deanchristt/order-service/dto"
 	"github.com/deanchristt/order-service/entity"
 	"github.com/deanchristt/order-service/helper"
@@ -55,6 +56,7 @@ func (auth *authController) Register(c *gin.Context) {
 	if errDto != nil {
 		response := helper.BuildErrorResponse("Failed to process request", errDto.Error(), helper.EmptyObj{})
 		c.AbortWithStatusJSON(http.StatusBadRequest, response)
+		fmt.Println("adwniwa")
 		return
 	}
 
